ab10e97ee65d87eb1824d79cab52f3c183609dab
logiclogue <jordanpaullord@hotmail.co.uk>
2019-09-20 23:23:21 +0100

    Gets the pixels to appear on the screen

--- src/main.ml
+++ src/main.ml
@@ -1,6 +1,15 @@
 let () =
     let canvas = RgbCanvas.create () in
 
-    let action = RgbCanvasAction.Start in
-
-    RgbCanvas.dispatch canvas action
+    RxJS.create_of_list [
+        EightColourCanvas.Clear;
+        EightColourCanvas.SetPixelXY (0, 0, 7);
+        EightColourCanvas.SetPixelXY (1, 1, 7);
+        EightColourCanvas.SetPixelXY (2, 2, 7);
+        EightColourCanvas.SetPixelXY (3, 3, 7);
+    ]
+        |> RxJS.map EightColourToRgbCanvas.f
+        |> RxJS.concat_list
+        |> RxJS.map (fun action -> [action; RgbCanvasAction.PutData])
+        |> RxJS.concat_list
+        |> RxJS.subscribe (RgbCanvas.dispatch canvas)
