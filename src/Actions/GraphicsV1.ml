a650f5b84c0f201ebeaf350a531913f4341a1aa7
logiclogue <jordanpaullord@hotmail.co.uk>
2019-10-21 22:41:43 +0100

    Adds graphics v1

---
+++ src/Actions/GraphicsV1.ml
@@ -0,0 +1,9 @@
+type id = string
+type coords = int * int
+
+type t =
+    | Clear
+    | SetSpriteSheet of id -> int list list
+    | SetFont of char -> int list list
+    | DrawSprite of id * coords
+    | DrawText of string * coords
