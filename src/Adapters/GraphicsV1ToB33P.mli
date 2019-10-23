7c0e9a68138025f61be6cf4f7c88932ec09f901a
logiclogue <jordanpaullord@hotmail.co.uk>
2019-10-23 22:34:43 +0100

    Adding graphics v1 to b33p

---
+++ src/Adapters/GraphicsV1ToB33P.mli
@@ -0,0 +1,9 @@
+type t = (GraphicsV1Entity.t -> GraphicsV1Entity.t) * SpriteCanvas.t
+
+val clear : t
+val set_sprite_sheet : (id -> int list list) -> t
+val set_font : (char -> int list list) -> t
+val draw_sprite : (id * coords) -> t
+val draw_text : (string * coords) -> t
+
+val f : GraphicsV1.t -> t
