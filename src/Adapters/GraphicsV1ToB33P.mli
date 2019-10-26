093a2c2bc88107b12c9af7f37b566fcd943e915b
logiclogue <jordanpaullord@hotmail.co.uk>
2019-10-26 18:36:19 +0100

    Updating

--- src/Adapters/GraphicsV1ToB33P.mli
+++ src/Adapters/GraphicsV1ToB33P.mli
@@ -1,4 +1,4 @@
-type t = (GraphicsV1Entity.t -> GraphicsV1Entity.t) * SpriteCanvas.t
+(*type t = (GraphicsV1Entity.t -> GraphicsV1Entity.t) * SpriteCanvas.t
 
 val clear : t
 val set_sprite_sheet : (id -> int list list) -> t
@@ -6,4 +6,4 @@ val set_font : (char -> int list list) -> t
 val draw_sprite : (id * coords) -> t
 val draw_text : (string * coords) -> t
 
-val f : GraphicsV1.t -> t
+val f : GraphicsV1.t -> t*)
--- src/main.ml
+++ src/main.ml
@@ -1,13 +1,27 @@
+let sprite_sheet id =
+    match id with
+    | "character" -> [
+        [0; 0; 0; 1; 1; 1; 1; 1; 0; 0; 0; 0];
+        [0; 0; 1; 1; 1; 1; 1; 1; 1; 1; 1; 0];
+        [0; 0; 2; 2; 2; 3; 3; 2; 3; 0; 0; 0];
+        [0; 2; 3; 2; 3; 3; 3; 2; 3; 3; 3; 0];
+        (* TODO: THE REST OF SPRITE *)
+    ]
+
 let () =
     let canvas = RgbCanvas.create () in
 
-    RxJS.create_of_list [
-        TextCanvas.DrawText ("ABCDEFG", 1, 1, 7);
+    RxJS.create_of_list GraphicsV1.[
+        Clear;
+        SetSpriteSheet sprite_sheet;
+        SetFont CharacterSprites.find;
+        DrawSprite ("character", (50, 30));
+        DrawText ("JORDAN", (50, 0));
     ]
-        |> RxJS.map TextToSpriteCanvas.f
+        (*|> RxJS.map TextToSpriteCanvas.f
         |> RxJS.concat_list
         |> RxJS.map SpriteToEightCanvas.f
-        |> RxJS.concat_list
+        |> RxJS.concat_list*)
         |> RxJS.map EightColourToRgbCanvas.f
         |> RxJS.concat_list
         |> RxJS.map (fun action -> [action; RgbCanvasAction.PutData])
