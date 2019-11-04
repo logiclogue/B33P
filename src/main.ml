7c3d3dae14dd111375d40f63ce71ffc561c87a2d
logiclogue <jordanpaullord@hotmail.co.uk>
2019-11-04 14:51:53 +0000

    Adding draw state (tick) function, enable more vebose logging in bsconfig

--- src/main.ml
+++ src/main.ml
@@ -19,24 +19,55 @@ let animations id =
 let actions_to_state (state : GraphicsV2State.t) (action : GraphicsV2.t) : GraphicsV2State.t =
     GraphicsV2State.(GraphicsV2.(
         match action with
-        | SetSpriteSheet sprite_sheet -> state
-        | SetFont font -> state
-        | SetAnimations animations -> set_animations animations state
-        | SetTextColour colour -> state
-        | CreateEntity (entity_id, coords) -> create_entity entity_id coords state
-        | SetAnimation (entity_id, animation_id) -> set_animation entity_id animation_id state
+        | SetSpriteSheet sprite_sheet ->
+            state
+
+        | SetFont font ->
+            state
+
+        | SetAnimations animations ->
+            set_animations animations state
+
+        | SetTextColour colour ->
+            state
+
+        | CreateEntity (entity_id, coords) ->
+            create_entity entity_id coords state
+
+        | SetAnimation (entity_id, animation_id) ->
+            set_animation entity_id animation_id state
     ))
 
 let action_to_actions (action : GraphicsV2.t) (state : GraphicsV2State.t) : GraphicsV1.t list =
     GraphicsV2.(
         match action with
-        | SetSpriteSheet sprite_sheet -> [GraphicsV1.SetSpriteSheet sprite_sheet]
-        | SetFont font -> [GraphicsV1.SetFont font]
-        | SetAnimations _ -> []
-        | SetTextColour colour -> [GraphicsV1.SetTextColour colour]
-        | CreateEntity _ -> []
-        | SetAnimation _ -> []
+        | SetSpriteSheet sprite_sheet ->
+            [GraphicsV1.SetSpriteSheet sprite_sheet]
+
+        | SetFont font ->
+            [GraphicsV1.SetFont font]
+
+        | SetAnimations _ ->
+            []
+
+        | SetTextColour colour ->
+            [GraphicsV1.SetTextColour colour]
+            
+        | CreateEntity _ ->
+            []
+
+        | SetAnimation _ ->
+            []
     )
+
+let draw_state (state : GraphicsV2State.t) (time : int) : GraphicsV1.t list =
+    let draw_entity ((animation_id, (x, y)) : (string * (int * int))) : GraphicsV1.t list =
+        [] in (* TODO *)
+
+    let draw_entities (entities : (string * (int * int)) list) : GraphicsV1.t list =
+        [] in
+
+    draw_entities (GraphicsV2State.get_entities state)
 
 let graphics_v2_to_graphics_v1_obs (graphics_v2_obs : GraphicsV2.t RxJS.observable) : GraphicsV1.t RxJS.observable =
     let f ((_, old_state) : (GraphicsV1.t list * GraphicsV2State.t)) (action : GraphicsV2.t) (_ : int) =
