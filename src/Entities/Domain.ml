e60027601b73799c3cbffd8543aac3c641a34f1a
logiclogue <jordanpaullord@hotmail.co.uk>
2019-11-29 17:13:30 +0000

    Updates Domain

--- src/Entities/Domain.ml
+++ src/Entities/Domain.ml
@@ -13,6 +13,16 @@ type entity_attributes =
 type entity_id = string
 type entity = coords * entity_attributes
 
+type scene_state = {
+    entities: entity list;
+}
+
+type game_state = {
+    time: milliseconds;
+    current_scene: scene_state;
+    scenes: scene_state list;
+}
+
 type game_update = game_state -> game_state
 type scene_update = scene_state -> scene_state
 type entity_update = entity -> entity
@@ -23,13 +33,11 @@ type action =
     | EntityUpdate of entity_update
     | SwitchScene of scene_id
 
-type scene_state = {
+type scene_seed = {
     entities: entity list;
-    update: entity -> scene_state -> action
+    update: scene_state -> action
 }
 
-type game_state = {
-    time: milliseconds;
-    current_scene: scene_state;
-    scenes: scene_state list;
+type game_seed = {
+    scenes: scene_seed list;
 }
